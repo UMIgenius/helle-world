28ba0dfaad88f95e26bef3b31b56ce5165c4f62c
Ymato Shigyou <yamato0088800@gmail.com>
2022-06-12 20:27:04 +0900

    修正

---
+++ jsetc3.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector('button');
+let elem = document.querySelector('p');
+let mes = 100;
+
+btn.addEventListener('click',() => {
+    elem,innerText += mes;
+    
+    for(i=0 ; i<10 ; i+=1 ){
+        mes +=150;
+        elem.innerText += " → " + mes;
+    }
+} );
